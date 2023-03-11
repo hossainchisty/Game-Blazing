8627d9bbfe5c9016047a8d6eb08441ae581197c3
hossainchisty <hossain.chisty11@gmail.com>
2023-03-11 14:05:27 +0600

    Initial release

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import GameCard from "./components/GameCard";
+import SearchIcon from "./search.svg";
+import "./style/App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [games, setGames] = useState("");
+  const fetchGame = async () => {
+    const response = await fetch(
+      `https://api.rawg.io/api/games?key=b5059cddfae7423aa14e4ed007adc0c9`
+    );
+    const data = await response.json();
+    setGames(data.results);
+  };
+
+  useEffect(() => {
+    fetchGame();
+  }, []);
+  return <div className="app">
+      <h1>Game Play Blazing</h1>
+
+      <div className="search">
+        <input
+          placeholder={`Search ${games.length} games`}
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+        />
+      </div>
+    
+       {games?.length > 0 ? (
+        <div className="container">
+          {games.map((game) => (
+            <GameCard game={game} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No games found</h2>
+        </div>
+      )}
+  </div>;
 }
 
 export default App;
---
+++ src/components/GameCard.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function GameCard({ game }) {
+  return (
+    <div className="game">
+      <img src={game.background_image} alt={game.name} />
+      <h3>{game.name}</h3>
+      <div className="game-info">
+        <p>Release date: {game.released}</p>
+        <p>Rating top: {game.rating_top}</p>
+        <p>Metascore: {game.rating}</p>
+        <p>Genres: {game.genres.map((genre) => genre.name).join(", ")}</p>
+        <p>
+          Platforms:{" "}
+          {game.platforms.map((platform) => platform.platform.name).join(", ")}
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default GameCard;
